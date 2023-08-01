79aae60b75407be5b750295c64e09672988da048
Daniel Cardoso Carneiro <daniel.carneiro@engenhos.net>
2023-08-01 00:34:43 -0300

    teste

--- Daniel.AulaEnsino.Core.Data/Context/MeuContextoBD.cs
+++ Daniel.AulaEnsino.Core.Data/Context/MeuContextoBD.cs
@@ -5,7 +5,7 @@ namespace Daniel.AulaEnsino.Core.Data.Context
     public class MeuContextoBD : DbContext
     {
         #region Constructor
-        //teste
+        //teste2
 
         #endregion
     }
